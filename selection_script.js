c4d6aede5b4d84174c6ae7c7d577315b91bd3843
nilaci <34284678+nilaci@users.noreply.github.com>
2019-07-21 14:03:33 +0300

    Create selection_script.js
    Selection script.
    
    How it works?
    - the script activates on changing the tools to/from one of the selection tools.
    - if a pixel is selected when the activation event occurs, it will create and 7x7, 15x15, 21x21 px selection which will save as .PNG file.
    
    TBD
    - add user input option of the selection size.
    - add user input option of the save path (currently the new images are saved in the original document directory).
    - split code into functions.

---
+++ selection_script.js
@@ -0,0 +1,53 @@
+
+var docRef = app.activeDocument;
+var origDoc = app.activeDocument;
+
+var rulerUnits = app.preferences.rulerUnits;
+var typeUnits = app.preferences.typeUnits;
+
+app.preferences.rulerUnits = Units.PIXELS;
+app.preferences.typeUnits = TypeUnits.PIXELS;
+
+var position = docRef.selection.bounds;
+
+docRef.selection.deselect();
+
+var i = 0;
+var x_k = position[0];
+var y_k = position[1];
+var selSze = [7, 15, 21];
+
+for (i = 0; i < selSze.length; i++) {
+	var floor = Math.floor(selSze[i]/2);	
+	var ceil = Math.ceil(selSze[i]/2);
+	var x = x_k - floor; // x pos from top left of future selection
+	var y = y_k - floor; // y pos from top left of future selection
+	var bounds = [[x, y], [x, y+selSze[i]], [x+selSze[i], y+selSze[i]], [x+selSze[i], y]];
+	docRef.selection.select(bounds);
+	
+	docRef.selection.copy(true);
+	var width = new UnitValue(selSze[i], 'px');
+	var height = new UnitValue(selSze[i], 'px');
+	var newDoc = documents.add(width, height, 72, 'sc_'+selSze[i], NewDocumentMode.GRAYSCALE);
+	newDoc.paste();
+	newDoc.layers.getByName("Background").remove();
+	docRef.activeDocument = newDoc;
+	
+	var fileName = "bifurcation_"+selSze[i]+"x"+selSze[i];
+	var filePath = origDoc.path;
+	var saveFile = File(filePath + "/" + fileName);
+	if(saveFile.exists) saveFile.remove();
+	activeDocument.saveAs(saveFile, new PNGSaveOptions(), true, Extension.LOWERCASE);
+	
+	app.activeDocument.close(SaveOptions.DONOTSAVECHANGES);
+	
+	docRef = origDoc;
+}
+
+docRef.selection.deselect();
+
+alert("Done with the selected area!");
+
+app.preferences.rulerUnits = rulerUnits;
+app.preferences.typeUnits = typeUnits;
+
