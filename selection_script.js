498463256324e34c019da9afd6c4cdd17057f206
nilaci <34284678+nilaci@users.noreply.github.com>
2019-07-25 15:21:55 +0300

    added save folder prompt and type of fg feature

--- selection_script.js
+++ selection_script.js
@@ -1,6 +1,5 @@
 // Special thanks to: https://gist.github.com/opdude/3826350
 
-
 var docRef = app.activeDocument;
 var origDoc = app.activeDocument;
 
@@ -10,49 +9,52 @@ var typeUnits = app.preferences.typeUnits;
 app.preferences.rulerUnits = Units.PIXELS;
 app.preferences.typeUnits = TypeUnits.PIXELS;
 
-var position = docRef.selection.bounds;
+try {
+	var position = docRef.selection.bounds;
 
-docRef.selection.deselect();
+	docRef.selection.deselect();
 
-var i = 0;
-var x_k = position[0];
-var y_k = position[1];
-var selSze = [7, 15, 21];
+	var i = 0;
+	var x_k = position[0];
+	var y_k = position[1];
+	var selSze = [7, 15, 21];
 
-var basePath = Folder.selectDialog("Select folder to save the images");
-var suffix = prompt("Please provide the type of fingerprint feature you are going to save...")
-var featurePath = basePath+"/"+suffix
+	var basePath = Folder.selectDialog("Select folder to save the images");
+	var featureType = prompt("Please provide the type of fingerprint feature you are going to save (default: termination)", "termination")
 
-for (i = 0; i < selSze.length; i++) {
-	var floor = Math.floor(selSze[i]/2);	
-	var x = x_k - floor; // x pos from top left of future selection
-	var y = y_k - floor; // y pos from top left of future selection
-	var bounds = [[x, y], [x, y+selSze[i]], [x+selSze[i], y+selSze[i]], [x+selSze[i], y]];
-	docRef.selection.select(bounds);
-	
-	docRef.selection.copy(true);
-	var width = new UnitValue(selSze[i], 'px');
-	var height = new UnitValue(selSze[i], 'px');
-	var newDoc = documents.add(width, height, 72, 'sc_'+selSze[i], NewDocumentMode.GRAYSCALE);
-	newDoc.paste();
-	newDoc.layers.getByName("Background").remove();
-	docRef.activeDocument = newDoc;
-	
-	var filePath = featurePath+"/"+selSze[i]+"x"+selSze[i];
-	var startIdx = Folder(filePath).getFiles().length+1;
-	var fileName = ""+startIdx;
-	var saveFile = File(filePath + "/" + fileName);
-	activeDocument.saveAs(saveFile, new PNGSaveOptions(), true, Extension.LOWERCASE);
-	
-	app.activeDocument.close(SaveOptions.DONOTSAVECHANGES);
-	
-	docRef = origDoc;
+	for (i = 0; i < selSze.length; i++) {
+		var floor = Math.floor(selSze[i]/2);	
+		var x = x_k - floor; // x pos from top left of future selection
+		var y = y_k - floor; // y pos from top left of future selection
+		var bounds = [[x, y], [x, y+selSze[i]], [x+selSze[i], y+selSze[i]], [x+selSze[i], y]];
+		docRef.selection.select(bounds);
+		
+		docRef.selection.copy(true);
+		var width = new UnitValue(selSze[i], 'px');
+		var height = new UnitValue(selSze[i], 'px');
+		var newDoc = documents.add(width, height, 72, 'sc_'+selSze[i], NewDocumentMode.GRAYSCALE);
+		newDoc.paste();
+		newDoc.layers.getByName("Background").remove();
+		docRef.activeDocument = newDoc;
+		
+		var filePath = basePath+"/"+selSze[i]+"x"+selSze[i]+"/"+featureType;
+		var startIdx = Folder(filePath).getFiles().length+1;
+		var fileName = ""+startIdx;
+		//alert("filePath: "+filePath+" startIdx: "+startIdx+" fileName: "+fileName)
+		var saveFile = File(filePath + "/" + fileName);
+		activeDocument.saveAs(saveFile, new PNGSaveOptions(), true, Extension.LOWERCASE);
+		
+		app.activeDocument.close(SaveOptions.DONOTSAVECHANGES);
+		
+		docRef = origDoc;
+	}
+
+	docRef.selection.deselect();
+
+	alert("Success!");
+
+	app.preferences.rulerUnits = rulerUnits;
+	app.preferences.typeUnits = typeUnits;
+} catch(err) {
+	// do nothing
 }
-
-docRef.selection.deselect();
-
-alert("Done with the selected area!");
-
-app.preferences.rulerUnits = rulerUnits;
-app.preferences.typeUnits = typeUnits;
-
